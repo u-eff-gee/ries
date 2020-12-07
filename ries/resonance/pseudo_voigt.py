Correct numerical PPF of pseudo Voigt
- Previously, I had misinterpreted the output of scipy.optimize.newton.
- Fallback solution only needed in extreme cases.
- Use centroid as start value: safer and faster.
- Dedicated test for Voigt PPF.

ries/resonance/pseudo_voigt.py
@@ -202,9 +202,9 @@ class PseudoVoigtDistribution:
         """PPF of the pseudo-Voigt distribution
 
         This function tries to invert the pseudo-Voigt CDF numerically first.
-        If the root-finding algorithm does not converge for any of the given quantiles, a warning
-        is issued and a fallback approximation is used which is based on a linear combination of
-        PPFs.
+        If the root-finding algorithm does not converge for any of the given quantiles, the 
+        RuntimeError issued by the algorithm is caught and a fallback approximation is used which 
+        is based on a linear combination of PPFs.
 
         Parameters:
 
@@ -214,32 +214,29 @@ class PseudoVoigtDistribution:
 
         float or array_like, PPF
         """
-        warnings.filterwarnings("error")
+
         try:
-            root, r, converged = newton(
-                lambda E: self.cdf(E) - quantile,
-                0.5
-                * (
-                    norm.ppf(
-                        quantile,
-                        loc=self.resonance_energy,
-                        scale=self.gamma_G / np.sqrt(2.0),
-                    )
-                    + cauchy.ppf(
-                        quantile, loc=self.resonance_energy, scale=0.5 * self.gamma_L
-                    )
-                ),
-                fprime=lambda E: self.pseudo_voigt_expression(E, "pdf"),
-                full_output=True,
-            )
-            if False in converged:
-                warnings.warn(
-                    "Root-finding algorithm did not converge for at least one value.",
-                    RuntimeWarning,
-                )
-            return root
-        except RuntimeWarning:
-            warnings.filterwarnings("default")
+            # scipy.optimize.newton returns different output depending on whether quantile is a 
+            # scalar or an array.
+            # For a scalar, the root is `newton_result[0]` and the flag that indicates whether 
+            # the algorithm converged is `newton_result[1]`.converged`.
+            # In the case of an array, it is `newton_result[0]` and `newton_result[1]`.
+            newton_result = newton(
+            lambda E: self.pseudo_voigt_expression(E, "cdf") - quantile,
+            # Use resonance energy as start value.
+            # There are closer guesses for an arbitrary q, but starting at the resonance 
+            # energy ensures that the algorithm does not have rounding errors from the start 
+            # which might cause it to go in the wrong direction.
+            self.resonance_energy if isinstance(quantile, (float, int)) else self.resonance_energy*np.ones(len(quantile)),
+            fprime=lambda E: self.pseudo_voigt_expression(E, "pdf"),
+            full_output=True) 
+            if isinstance(quantile, (int, float)):
+                if newton_result[1].converged:
+                    return newton_result[0]
+            elif False not in newton_result[1]:
+                return newton_result[0]
+
+        except RuntimeError:
             warnings.warn(
                 "Calculation of pseudo-Voigt PPF by numerical inversion of the CDF failed for at least one value. Using approximation instead.",
                 UserWarning,

tests/test_resonance_models.py
@@ -68,11 +68,7 @@ class TestResonanceModels:
     )
     def test_coverage(self, Model, parameters, rtol):
         cs = Model(B11.ground_state, B11.excited_states["5/2^-_1"], *parameters)
-        if Model in (PseudoVoigt, Voigt):
-            with pytest.warns(UserWarning):
-                cov_int = cs.coverage_interval(0.5)
-        else:
-            cov_int = cs.coverage_interval(0.5)
+        cov_int = cs.coverage_interval(0.5)
 
         assert np.isclose(
             quad(cs, cov_int[0], cov_int[1])[0],
@@ -82,13 +78,12 @@ class TestResonanceModels:
 
     def test_limits(self):
         voigt = Voigt(B11.ground_state, B11.excited_states["5/2^-_1"], B11.amu, 1e-3)
-        with pytest.warns(UserWarning):
-            energy = voigt.equidistant_probability_grid(0.95, 100)
-            assert np.allclose(
-                voigt.equidistant_probability_grid((energy[0], energy[-1]), 100),
-                energy,
-                rtol=1e-5,
-            )
+        energy = voigt.equidistant_probability_grid(0.7, 100)
+        assert np.allclose(
+            voigt.equidistant_probability_grid((energy[0], energy[-1]), 100),
+            energy,
+            rtol=1e-5,
+        )
 
         breit_wigner = BreitWigner(
             B11.ground_state,
@@ -97,8 +92,7 @@ class TestResonanceModels:
         assert np.allclose(voigt(energy), breit_wigner(energy), rtol=1e-2)
 
         voigt = Voigt(B11.ground_state, B11.excited_states["5/2^-_1"], B11.amu, 1e4)
-        with pytest.warns(UserWarning):
-            energy = voigt.equidistant_probability_grid(0.95, 100)
+        energy = voigt.equidistant_probability_grid(0.95, 100)
 
         gauss = Gauss(B11.ground_state, B11.excited_states["5/2^-_1"], B11.amu, 1e4)
         assert np.allclose(voigt(energy), gauss(energy), rtol=5e-2)
@@ -108,9 +102,8 @@ class TestResonanceModels:
         )
 
         assert np.allclose(voigt(energy), pseudo_voigt(energy), rtol=1e-2)
-        with pytest.warns(UserWarning):
-            assert np.allclose(
-                pseudo_voigt.equidistant_probability_grid((energy[0], energy[-1]), 100),
-                energy,
-                rtol=1e-5,
-            )
+        assert np.allclose(
+            pseudo_voigt.equidistant_probability_grid((energy[0], energy[-1]), 100),
+            energy,
+            rtol=1e-5,
+        )