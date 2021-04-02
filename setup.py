42f18997e727028620f8997b7aebddf4b6b6c20e
Udo Friman-Gayer <ufg@email.unc.edu>
2021-04-02 19:25:42 -0400

    Notebook for 6Li RSA experiment
    - Rename compound-target notebook

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import setuptools
 
 setuptools.setup(
     name="ries",
-    version="0.2.0",
+    version="0.2.1",
     description="resonances integrated over energy and space",
     author="Udo Friman-Gayer",
     author_email="ufg@email.unc.edu",
